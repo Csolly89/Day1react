5a291897b87628ad1be7121d3755ff9c181228fd
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-11-15 21:08:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const [name, setName] = useState('Arnold')
+
+  // setTimeout(() => {
+  //   setName('charles')
+  // }, 3500)
+
+  // const people = ['Nick', 'Cjay',"Thomas"]
+  // const me = 'Arnold'
+  // const display = people.map(person => {
+  //   return <p>{person}</p>
+  // })
+
+  const [counter,setCounter] = useState(0)
+  
+  const increment = () => {
+    setCounter(currVal => {
+      setCounter(currVal +1)
+    })
+  }
+  const decrease = () => {
+    if(counter === 0) return
+    setCounter(currVal => {
+      setCounter(currVal -1)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>GET TO THA CHOPPAH </h1>
+      <p>counter: {counter}</p>
+      <button onClick={decrease}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
 
 export default App;
+// onClick, onChange, onSubmit
+// dont manipulate the DOM directly
+//  dont ever ever ever ever change a stateful variable directly
